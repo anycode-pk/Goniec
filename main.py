471eaa687d5a192deaa12a4fb7c4633a7dc3183d
Paul Frank <pawelspam42@gmail.com>
2023-09-02 19:13:37 +0200

    Added logging

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import settings
 import discord
 from discord.ext import commands
 
+logger = settings.logging.getLogger("bot")
+
 def run():
+    print("Hello")
     intents = discord.Intents.default()
     bot = commands.Bot(command_prefix="!",intents=intents)
+    #client = discord.Client()
 
     @bot.event
     async def on_ready():
-        print(bot.user)
-        print(bot.owner_id)
-        print(bot.user.id)
-        print("_____________-")
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+        logger.info("______________")
+        #logger.info('Logged as client: {0.user}'.format(client))
 
-    bot.run(settings.DISCORD_API_SECRET)
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+    #client.run(settings.DISCORD_API_SECRET)
 
 if __name__ == "__main__":
     run()
+    
