45fd2a25cbab0a03981912cda2c18fc816f38ef0
Paul Frank <pawelspam42@gmail.com>
2023-09-06 23:51:54 +0200

    Zabawa z message event

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import settings
-import discord
+import settings, discord
 from discord.ext import commands
 
 logger = settings.logging.getLogger("bot")
@@ -7,17 +6,25 @@ logger = settings.logging.getLogger("bot")
 def run():
     print("Hello")
     intents = discord.Intents.default()
-    bot = commands.Bot(command_prefix="!",intents=intents)
-    #client = discord.Client()
+    intents.message_content = True
+
+    bot = commands.Bot(command_prefix="$",intents=intents)
 
     @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
         logger.info("______________")
-        #logger.info('Logged as client: {0.user}'.format(client))
+
+    @bot.event
+    async def on_message(message):
+        logger.info(f'Message from {message.author}: {message.content}')
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+    
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
-    #client.run(settings.DISCORD_API_SECRET)
 
 if __name__ == "__main__":
     run()
